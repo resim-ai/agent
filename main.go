b02c240d66b5b69fb4aaaf2960a91357a3f0a870
Ben Abineri <ben@resim.ai>
2024-08-27 17:32:15 +0000

    remove dummy values

--- main.go
+++ main.go
@@ -71,23 +71,23 @@ func Start(agent Agent) {
 
 	agent.startHeartbeat()
 
-	// ctx := context.Background()
+	ctx := context.Background()
 
 	for {
 		agent.checkAuth()
 
-		// job := agent.getJob()
-		// agent.pullImage(ctx, job.WorkerImageURI)
+		job := agent.getJob()
+		agent.pullImage(ctx, job.WorkerImageURI)
+		agent.pullImage(ctx, job.CustomerImageURI)
+
+		// loop through env vars and find RERUN_WORKER_BUILD_IMAGE_URI
 		// agent.pullImage(ctx, job.CustomerImageURI)
 
-		// // loop through env vars and find RERUN_WORKER_BUILD_IMAGE_URI
-		// // agent.pullImage(ctx, job.CustomerImageURI)
-
-		// customerContainerID := agent.createCustomerContainer(job)
-		// err := agent.runCustomerContainer(ctx, customerContainerID)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
+		customerContainerID := agent.createCustomerContainer(job)
+		err := agent.runCustomerContainer(ctx, customerContainerID)
+		if err != nil {
+			log.Fatal(err)
+		}
 		time.Sleep(50 * time.Second)
 	}
 }
@@ -203,12 +203,6 @@ func stringifyEnvironmentVariables(inputVars [][]string) []string {
 	for _, envVar := range inputVars {
 		envVarString := strings.Join(envVar, "=")
 
-		if envVar[0] == "RERUN_WORKER_AUTH_TOKEN" {
-			envVarString = fmt.Sprintf("%v={\"access_token\":\"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlU5cm5vRXhFWV9PSi1pT1lncmg5TiJ9.eyJodHRwczovL2FwaS5yZXNpbS5haS9vcmdfaWQiOiJlMmUucmVzaW0uYWkiLCJodHRwczovL2FwaS5yZXNpbS5haS9lbWFpbCI6IndvcmtlcitlMmUucmVzaW0uYWlAcmVzaW0uYWkiLCJodHRwczovL2FwaS5yZXNpbS5haS93b3JrZXJfdHlwZSI6InVuaXZlcnNhbCIsImlzcyI6Imh0dHBzOi8vcmVzaW0tZGV2LnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJjN2lOUGh0clJ0OTR5R3VkWVA1ZlZSbzh1M2RqTEVPOEBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9hcGkucmVzaW0uYWkiLCJpYXQiOjE3MjM3MzY2NTIsImV4cCI6MTcyMzgyMzA1Miwic2NvcGUiOiJ3b3JrZXIgZXhwZXJpZW5jZXM6cmVhZCBleHBlcmllbmNlczp3cml0ZSBleHBlcmllbmNlVGFnczpyZWFkIGV4cGVyaWVuY2VUYWdzOndyaXRlIHByb2plY3RzOnJlYWQgcHJvamVjdHM6d3JpdGUgYmF0Y2hlczpyZWFkIGJhdGNoZXM6d3JpdGUgYnVpbGRzOnJlYWQgYnVpbGRzOndyaXRlIHZpZXc6cmVhZCB2aWV3OndyaXRlIHN5c3RlbXM6cmVhZCBzeXN0ZW1zOndyaXRlIHN3ZWVwczpyZWFkIHN3ZWVwczp3cml0ZSByZXBvcnRzOnJlYWQgcmVwb3J0czp3cml0ZSIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsImF6cCI6ImM3aU5QaHRyUnQ5NHlHdWRZUDVmVlJvOHUzZGpMRU84IiwicGVybWlzc2lvbnMiOlsid29ya2VyIiwiZXhwZXJpZW5jZXM6cmVhZCIsImV4cGVyaWVuY2VzOndyaXRlIiwiZXhwZXJpZW5jZVRhZ3M6cmVhZCIsImV4cGVyaWVuY2VUYWdzOndyaXRlIiwicHJvamVjdHM6cmVhZCIsInByb2plY3RzOndyaXRlIiwiYmF0Y2hlczpyZWFkIiwiYmF0Y2hlczp3cml0ZSIsImJ1aWxkczpyZWFkIiwiYnVpbGRzOndyaXRlIiwidmlldzpyZWFkIiwidmlldzp3cml0ZSIsInN5c3RlbXM6cmVhZCIsInN5c3RlbXM6d3JpdGUiLCJzd2VlcHM6cmVhZCIsInN3ZWVwczp3cml0ZSIsInJlcG9ydHM6cmVhZCIsInJlcG9ydHM6d3JpdGUiXX0.ZAydfqgKNnHbC0KbyQh1gHwQrtRM9v794QslK1pfwGNWAgdZuTKkpKaYFdEClSdU3R5hwo16GVMqXB4BRIcPc09AU2fcX0qk5oGGdFucIuSBG50GMnCBCGWtWx9Jnl7FCqHoncsJEjf-b-E2Y35anIJDcyjN0j2_3szQW7-dDp4v7Q9J8nSl2YdW-zxG3FscYkLeAzCwJf_ESHpCFtKCmsw1fo6V_SCJojiU4EfFf2tDCMiyBpsIFHI5vHe7i68GIicGDGa6KDTcdNJY2ky58blL4Ie0tEbKMqol8tgqoyCdG3Zb48pU9ZG_S-C5FJ8R-Vr_ThFnjQT6LoCbbUV1LA\",\"token_type\":\"Bearer\",\"expiry\":\"2024-08-16T15:44:12.41592629Z\"}", envVar[0])
-		}
-		if envVar[0] == "RERUN_WORKER_BUILD_IMAGE_URI" {
-			envVarString = fmt.Sprintf("%v=public.ecr.aws/docker/library/hello-world:latest", envVar[0])
-		}
 		envVars = append(envVars, envVarString)
 	}
 	return envVars
