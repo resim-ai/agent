c6fe5ec8deff8e9eea0533895bf7ce2dc803e8e3
Andy Cowley <andy@resim.ai>
2024-10-09 15:12:16 +0100

    Provider config

--- test/integration/instance.tf
+++ test/integration/instance.tf
@@ -13,6 +13,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.0"
+    }
+    # FIXME: Remove this after the resource has been updated
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
   }
 }
 
