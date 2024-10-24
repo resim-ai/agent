f0c0feb7e21443fb03175e5698b40a6b3b6fb1f0
Ben Abineri <ben@resim.ai>
2024-10-24 13:40:07 +0000

    remove commented code

--- update.go
+++ update.go
@@ -82,13 +82,6 @@ func doUpdate(release *github.RepositoryRelease) error {
 		os.Exit(0)
 	}
 
-	// defer resp.Body.Close()
-	// err = selfupdate.Apply(resp.Body, selfupdate.Options{})
-	// if err != nil {
-	// 	slog.Error("error applying update", "err", err)
-	// 	return err
-	// }
-
 	return nil
 }
 
@@ -101,35 +94,6 @@ func (a *Agent) checkUpdate() error {
 		slog.Error("error getting latest release", "err", err)
 		return err
 	}
-
-	// client := http.Client{
-	// 	Timeout: 5 * time.Second,
-	// }
-
-	// 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
-	// req, _ := http.NewRequest("GET", latestReleaseURL, nil)
-
-	// REMOVE ONCE THE REPO IS PUBLIC
-	// req.Header.Set("Authorization", "Bearer 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")
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	slog.Error("error checking for releases", "err", err)
-	// 	return err
-	// }
-
-	// // var latestRelease githubRelease
-
-	// bodyData, err := io.ReadAll(resp.Body)
-	// if err != nil {
-	// 	slog.Error("error reading release response body", "err", err)
-	// 	return err
-	// }
-
-	// err = json.Unmarshal(bodyData, &latestRelease)
-	// if err != nil {
-	// 	slog.Error("error unmarshalling release response", "err", err)
-	// 	return err
-	// }
 
 	switch semver.Compare(agentVersion, *latestRelease.Name) {
 	case -1:
